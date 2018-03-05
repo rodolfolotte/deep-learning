e248c10bbe4a9e434c7b853867e1889dbabc4dc9
Rodolfo Lotte <rodolfo.lotte@gmail.com>
2018-03-05 12:38:11 -0300

    modification on run-segment.sh to get more than one folder

--- cnn-projects/multiclass-seg/run-segment.sh
+++ cnn-projects/multiclass-seg/run-segment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-for entry in RUNS/*
-do
-        python segment.py --input_folder /home/stefan/Desktop/ruemonge2014/dataset --output_folder "$entry"/segmentation/ --logdir "$entry"
+for entry in $1; do
+        if [ ! -d "$entry"/segmentation/ ]; then
+                 python segment.py --input_folder $2 --output_folder "$entry"/segmentation/ --logdir "$entry"
+        fi
 done
-
